bdf004abb6f60c6de0608d99e2be3e88b178e767
Yukun He <yukunhe@berkeley.edu>
2016-10-21 00:54:10 -0700

    fitted test set and full data

--- code/scripts/lasso-regression.R
+++ code/scripts/lasso-regression.R
@@ -1,9 +1,10 @@
 library("glmnet")
 
 train.set = read.csv(file = "data/data-sets/train-set.csv", header = T)
+test.set = read.csv(file = "data/data-sets/test-set.csv", header = T)
 
 dependent <- as.matrix(train.set$Balance)
-regressor <- as.matrix(train.set[c(-1, -13)])
+regressor <- as.matrix(train.set[c(-1,-13)])
 
 grid <- 10^seq(10, -2, length = 100)
 set.seed(1234)
@@ -18,3 +19,11 @@ png('images/regression-plot/lasso-regression-plot.png')
 plot(lasso.mod, main = "Lasso Regression Plot")
 dev.off()
 
+lasso.test.predict=predict(lasso.mod ,s=small.lambda.index, newx=as.matrix(test.set[c(-1,-13)]))
+lasso.test = as.matrix(test.set[,13])
+MSE_lasso = mean(sum(lasso.test - lasso.test.predict)^2)
+
+dependent_full = as.matrix(scaled_credit$Balance)
+regressor_full = as.matrix(scaled_credit[c(-1,-13)])
+lasso.full.fit = glmnet(regressor_full, dependent_full, alpha = 1, lambda = small.lambda.index, intercept = FALSE, standardize = FALSE)
+fitted_coef = coef(lasso.full.fit)
