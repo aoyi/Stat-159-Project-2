2a9f2b6f8af9b574f362124d0a9be64a64d20173
Aoyi <aoyi95@berkeley.edu>
2016-10-21 22:32:43 -0700

    Apply ols on train and test data

--- code/scripts/clean-data.R
+++ code/scripts/clean-data.R
@@ -4,9 +4,9 @@ new_credit <- cbind(temp_credit[ ,-1], Balance = credit$Balance)
 scaled_credit <- scale(new_credit, center = TRUE, scale = TRUE)
 write.csv(scaled_credit, file = "data/data-sets/scaled-credit.csv")
 
-set.seed(123)
+set.seed(12345)
 train.sample = sample(1:400, 300, replace = F)
-train.set = credit[train.sample, ]
-test.set = credit[-train.sample, ]
+train.set = scaled_credit[train.sample, ]
+test.set = scaled_credit[-train.sample, ]
 write.csv(train.set, file = "data/data-sets/train-set.csv")
 write.csv(test.set, file = "data/data-sets/test-set.csv")
